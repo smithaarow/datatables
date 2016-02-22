7fc122c40b084c86569bdf7c9b6db3ff3553fdc2
Jason Mosley <jason.mosley@domo.com>
2016-02-22 14:40:58 -0700

    CSS, code cleanup, toggle dates, increase card size

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 domo.get('/data/v1/udon_tracker')
     .then(function(udon_tracker){
       console.log("udon_tracker", udon_tracker);
-    
+
+        // Initialize Datatable
         var table = $('#example').DataTable( {
             scrollY:        '75vh',
             scrollCollapse: true,
@@ -53,7 +54,7 @@ domo.get('/data/v1/udon_tracker')
                 {
                     data: "is_shipstopper",
                     class: "is_shipstopper",
-                    title: "ship stopper"
+                    title: "Ship Stopper"
                 },
           
                 {
@@ -67,139 +68,46 @@ domo.get('/data/v1/udon_tracker')
                 {
                     data: "summary",
                     class: "summary",
-                    title: "domoSummary",
+                    title: "Summary",
                     "render": function ( data, type, full, meta ) {
                         return '<div class="container-overflowing">'+data+'</div>';
                     },
                 },
-                {
-                    data: "Fix Version/s" ,
-                    class: "fix-versions" ,
-                    title: "Fix Version"
-                },
-                {
-                    data: "2/8" ,
-                    title: "2/8"
-                },
-                {
-                    data: "2/9",
-                    title: "2/9"
-                },
-                {
-                    data: "2/10",
-                    title: "2/10"
-                },
-                {
-                    data: "2/11",
-                    title: "2/11"
-                },
-                {
-                    data: "2/12",
-                    title: "2/12"
-                },
-                {
-                    data: "2/15",
-                    title: "2/15"
-                },
-                {
-                    data: "2/16",
-                    title: "2/16"
-                },
-                {
-                    data: "2/17",
-                    title: "2/17"
-                },
-                {
-                    data: "2/18",
-                    title: "2/18"
-                },
-                {
-                    data: "2/19",
-                    title: "2/19"
-                },
-                {
-                    data: "2/23",
-                    title: "2/23"
-                },
-                {
-                    data: "2/24",
-                    title: "2/24"
-                },
-                {
-                    data: "2/25",
-                    title: "2/25"
-                },
-                {
-                    data: "2/26",
-                    title: "2/26"
-                },
-                {
-                    data: "2/29",
-                    title: "2/29"
-                },
-                {
-                    data: "3/1",
-                    title: "3/1"
-                },
-                {
-                    data: "3/2",
-                    title: "3/2"
-                },
-                {
-                    data: "3/3",
-                    title: "3/3"
-                },
-                {
-                    data: "3/4",
-                    title: "3/4"
-                },
-                {
-                    data: "3/7",
-                    title: "3/7"
-                },
-                {
-                    data: "3/8",
-                    title: "3/8"
-                },
-                {
-                    data: "3/9",
-                    title: "3/9"
-                },
-                {
-                    data: "3/10",
-                    title: "3/10"
-                },
-                {
-                    data: "3/11",
-                    title: "3/11"
-                },
-                {
-                    data: "3/14",
-                    title: "3/14"
-                },
-                {
-                    data: "3/15",
-                    title: "3/15"
-                },
-                {
-                    data: "3/16",
-                    title: "3/16"
-                },
-                {
-                    data: "3/17",
-                    title: "3/17"
-                },
-                {
-                    data: "3/18",
-                    title: "3/18"
-                },
-                {
-                    data: "3/21",
-                    title: "3/21"
-                },
+                { data: "Fix Version/s" , class: "fix-versions" , title: "Fix Version" },
+                { data: "2/8" , title: "2/8", visible: false, },
+                { data: "2/9", title: "2/9", visible: false, },
+                { data: "2/10", title: "2/10", visible: false, },
+                { data: "2/11", title: "2/11", visible: false, },
+                { data: "2/12", title: "2/12", visible: false,},
+                { data: "2/15", title: "2/15", visible: false, },
+                { data: "2/16", title: "2/16", visible: false, },
+                { data: "2/17", title: "2/17", visible: false, },
+                { data: "2/18", title: "2/18", visible: false, },
+                { data: "2/19", title: "2/19" , visible: false,},
+                { data: "2/23", title: "2/23" },
+                { data: "2/24", title: "2/24" },
+                { data: "2/25", title: "2/25" },
+                { data: "2/26", title: "2/26" },
+                { data: "2/29", title: "2/29" },
+                { data: "3/1", title: "3/1" },
+                { data: "3/2", title: "3/2" },
+                { data: "3/3", title: "3/3" },
+                { data: "3/4", title: "3/4" },
+                { data: "3/7", title: "3/7" },
+                { data: "3/8", title: "3/8" },
+                { data: "3/9", title: "3/9" },
+                { data: "3/10", title: "3/10" },
+                { data: "3/11", title: "3/11" },
+                { data: "3/14", title: "3/14" },
+                { data: "3/15", title: "3/15" },
+                { data: "3/16", title: "3/16" },
+                { data: "3/17", title: "3/17" },
+                { data: "3/18", title: "3/18" },
+                { data: "3/21", title: "3/21" },
             ]
         });
 
+        // Redraw index on every new sort or search
         table.on( 'order.dt search.dt', function () {
             table.column(1, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
                 cell.innerHTML = i+1;
@@ -207,8 +115,18 @@ domo.get('/data/v1/udon_tracker')
         } ).draw();
 
 
+        // Toggle visibility of columns
+        $('a.toggle-vis').on( 'click', function (e) {
+            e.preventDefault();
+
+            for ( var i=8 ; i<18 ; i++ ) {
+                //table.column( i ).visible( false, false );
+                table.column( i ).visible( ! table.column( i ).visible() );
+            }
+            table.columns.adjust().draw( false ); // adjust column sizing and redraw
+
+        } );
 
 } );
-
 
 
